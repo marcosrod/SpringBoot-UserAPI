470a90c704d63b4a2b065ac830523282eec99039
marcoskod <marcos-rodrigo05@hotmail.com>
2021-09-15 17:39:22 -0300

    Project Initial Setup

---
+++ src/test/java/com/marcosrod/SpringBootUserAPI/SpringBootUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosrod.SpringBootUserAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
